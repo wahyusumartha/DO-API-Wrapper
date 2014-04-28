ac4458c0df205f33d7340320e68a33e4b2010054
wahyu sumartha <wahyusumartha@gmail.com>
2014-04-28 22:07:08 +0800

    ^Create a macros for singleton class

---
+++ DOWrapper/DOClient.h
@@ -0,0 +1,13 @@
+//
+//  DOClient.h
+//  DOWrapper
+//
+//  Created by Wahyu Sumartha  on 4/28/14.
+//  Copyright (c) 2014 Wahyu Sumartha . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DOClient : NSObject
+
+@end
---
+++ DOWrapper/DOClient.m
@@ -0,0 +1,13 @@
+//
+//  DOClient.m
+//  DOWrapper
+//
+//  Created by Wahyu Sumartha  on 4/28/14.
+//  Copyright (c) 2014 Wahyu Sumartha . All rights reserved.
+//
+
+#import "DOClient.h"
+
+@implementation DOClient
+
+@end
